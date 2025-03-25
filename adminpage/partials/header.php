6af9f5c04165be628e00d0520097bf2f110d7ec2
Bayisa Daba <bayisa615@gmail.com>
2025-03-25 20:45:03 +0300

    feat: add new features and updates

---
+++ adminpage/partials/header.php
@@ -0,0 +1,18 @@
+<header class="header">
+		<a href="">Admin Dashboard</a>
+		<div class="logout">
+			<a href="logout.php" class="btn btn-primary">Logout</a>
+		</div>
+	</header>
+	<aside>
+		<ul>
+			<li><a href="addstudent.php">Add Student</a></li>
+			<li><a href="viewstudent.php">View Student</a></li>
+			<li><a href="addteacher.php">Add Teacher</a></li>
+			<li><a href="viewteacher.php">View Teacher</a></li>
+			<li><a href="addcourses.php">Add Courses</a></li>
+			<li><a href="viewcourses.php">View Courses</a></li>
+			<li><a href="addfamily.php">Add Family</a></li>
+			<li><a href="viewfamily.php">View Family</a></li>
+		</ul>
+	</aside>

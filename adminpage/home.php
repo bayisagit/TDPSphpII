a918d384508a816f88a21ff590127d0b68c74de3
Bayisa Daba <bayisa615@gmail.com>
2025-03-25 20:45:03 +0300

    feat: add new features and updates

---
+++ adminpage/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/adminpage.css">
+    <title>Admin Dashboard</title>
+</head>
+<body>
+	<?php
+	include("partials/header.php");
+	?>
+	<div class="content">
+		<h1>Sidebar Accordion</h1>
+		<p>In this example, we have added an accordion and a dropdown menu inside the side navigation.
+		Click on both to understand how they differ from each other. The accordion will push down the content, while the dropdown lays over the content.</p>
+		<input type="text" name="">
+	</div>
+</body>
+</html>

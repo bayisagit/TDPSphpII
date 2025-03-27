5ae2383a32dc61e407c56a8bd53bd050e1f33332
Bayisa Daba <bayisa615@gmail.com>
2025-03-27 20:27:12 +0300

    adding main admin pages

--- adminpage/addteacher.php
+++ adminpage/addteacher.php
@@ -3,15 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/adminpage.css">
+    <link rel="stylesheet" type="text/css" href="css/addteacher.css">
+    <link rel="stylesheet" href="partials/admin.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <title>Admin Dashboard</title>
 </head>
 <body>
     <?php
     include("partials/header.php");
     ?>
-	<div class="content">
-		<h1>add teacher</h1>
-	</div>
+    <main class="main-content">
+        <div>hello world</div>
+    </main>
 </body>
 </html>

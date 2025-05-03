c963fb71f1c8a32f516d928e4c420c8c4ea21068
beimnet melese <beimnetmelese16@gmail.com>
2025-05-03 18:35:23 +0300

    bug fixs

--- homepage/js/navigateyear.js
+++
@@ -1,8 +0,0 @@
-function navigateToYearTerm() {
-    const dropdown = document.getElementById("yearTermDropdown");
-    const selectedValue = dropdown.value;
-
-    if (selectedValue) {
-        window.location.href = selectedValue;
-    }
-}
